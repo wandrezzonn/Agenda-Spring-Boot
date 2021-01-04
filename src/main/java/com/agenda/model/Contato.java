c0a82a1ebfc15f1ba3a8c5c33231db05f99f59c2
Anderson Silva <anderson4602@gmail.com>
2021-01-04 14:24:35 -0300

    Cirado a classe Contato como entidade para o JPA

---
+++ src/main/java/com/agenda/model/Contato.java
@@ -0,0 +1,57 @@
+package com.agenda.model;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "contato")
+public class Contato {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "telefone")
+	private String telefone;
+	
+	public Contato() {
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+	
+
+}
