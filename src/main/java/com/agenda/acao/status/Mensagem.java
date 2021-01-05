dc867a9f63c11aa3a4ed5cf892047b41f66cb223
Anderson Silva <anderson4602@gmail.com>
2021-01-05 08:15:00 -0300

    Refatorado o Enum Mensagem

--- src/main/java/com/agenda/acao/status/Mensagem.java
+++ src/main/java/com/agenda/acao/status/Mensagem.java
@@ -1,11 +1,15 @@
 package com.agenda.acao.status;
 
 public enum Mensagem {
-/* Enum para envio de mensagem ao front de acordo com a ação da Classe ContatoValid*/
-	SUCESSO("Salvo com sucesso"),
+	/*
+	 * Enum para envio de mensagem ao front de acordo com a ação da Classe
+	 * ContatoValid
+	 */
+	SUCESSO("Salvo com sucesso"), 
 	VAZIO("Todos os campos devem ser preenchidos"),
-	DELETADO_SUCESSO("Deletado com sucesso"),
+	DELETADO_SUCESSO("Deletado com sucesso"), 
 	DELETADO_FALHA("Falha ao deletar");
+
 	private String mensagem;
 
 	private Mensagem(String mensagem) {
@@ -15,6 +19,5 @@ public enum Mensagem {
 	public String getMensagem() {
 		return mensagem;
 	}
-
 
 }
