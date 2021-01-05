d9b8170e2754e320902d4f28f4053910a7785251
Anderson Silva <anderson4602@gmail.com>
2021-01-05 09:43:56 -0300

    Ajuste nas anotações @

--- src/main/java/com/agenda/controller/AlterarController.java
+++ src/main/java/com/agenda/controller/AlterarController.java
@@ -3,11 +3,11 @@ package com.agenda.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.agenda.model.Contato;
@@ -17,6 +17,7 @@ import com.agenda.service.ContatoValid;
 
 
 @Controller
+@RequestMapping("/alterar")
 public class AlterarController {
 	@Autowired
 	private ContatoDAO dao;
@@ -24,7 +25,7 @@ public class AlterarController {
 	@Autowired
 	private ContatoValid valid;
 	
-	@RequestMapping(value = "/alterar/{id}", method = RequestMethod.GET)
+	@GetMapping(value = "/{id}")
 	public String alterar(@PathVariable("id") Integer id , Model model, Contato contato) {
 		contato = dao.buscarPorId(id);
 		model.addAttribute("contato", contato);
@@ -32,7 +33,7 @@ public class AlterarController {
 	}
 	
 	
-	@PostMapping(value = "/alterar/alterar")
+	@PostMapping(value = "/alterar")
 	public String alterar(@ModelAttribute Contato contato, RedirectAttributes mensagem) {
 		valid.validarSalvar(contato, mensagem);
 		return "redirect:/";
